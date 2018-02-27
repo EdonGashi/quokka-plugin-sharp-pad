b2bf4c113464c5e3320c89799a320ebaa97f318b
EdonGashi <edongashi@hotmail.co.uk>
2018-02-27 17:33:57 +0100

    Added source support

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ module.exports = {
   before: config => {
     const dump = require('sharp-pad-dump')
     dump.hook('$', true)
+    global.$ = '$'
     global.dump = dump
-    dump.source = false
+    dump.source = function (source, value, accessor) {
+      if (!source || source.length > 30) {
+        return null
+      }
+
+      if (value && value.$type === 'html') {
+        return null
+      }
+
+      const index = source.indexOf('[$]')
+      if (index !== -1) {
+        return source.substring(0, index).trim()
+      }
+
+      return source
+    }
+
     if (config && config['sharp-pad'] && config['sharp-pad'].port) {
       dump.port = config['sharp-pad'].port
     }
